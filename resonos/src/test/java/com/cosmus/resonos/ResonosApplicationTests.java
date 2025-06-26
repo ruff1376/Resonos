08f0ed8c11559d84433833e92a3de7cf98766bad
ruff1376 <ruff1376@gmail.com>
2025-06-26 15:21:40 +0900

    초기 세팅

---
+++ resonos/src/test/java/com/cosmus/resonos/ResonosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmus.resonos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResonosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
