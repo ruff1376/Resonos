2f8a67018e7b982176c2dc1209ee55a8fbfee412
Lee-0210 <tjsans9069@gmail.com>
2025-07-01 14:40:46 +0900

    시큐리티 관련 일단 주석 처리

--- resonos/src/main/java/com/cosmus/resonos/controller/UserController.java
+++ resonos/src/main/java/com/cosmus/resonos/controller/UserController.java
@@ -3,8 +3,11 @@ package com.cosmus.resonos.controller;
 import org.springframework.stereotype.Controller;
 
 import groovy.util.logging.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Slf4j
@@ -12,5 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-
+  @PostMapping("/login")
+  public String getMethodName(@RequestParam String param) {
+      return new String();
+  }
 }
