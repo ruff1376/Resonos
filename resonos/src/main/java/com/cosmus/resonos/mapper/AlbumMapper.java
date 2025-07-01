5a0c8a9e2e6d62146a029ad4e769b93fe1b5996f
ruff1376 <ruff1376@gmail.com>
2025-07-01 17:22:33 +0900

    음악 파트 매퍼, 서비스 / 헤더 진행중

--- resonos/src/main/java/com/cosmus/resonos/mapper/AlbumMapper.java
+++ resonos/src/main/java/com/cosmus/resonos/mapper/AlbumMapper.java
@@ -1,5 +1,15 @@
 package com.cosmus.resonos.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.cosmus.resonos.domain.Album;
+
+@Mapper
 public interface AlbumMapper {
-    
+    // 목록
+    public List<Album> list() throws Exception;
+    // 조회
+    public Album select(Integer no) throws Exception;
 }
