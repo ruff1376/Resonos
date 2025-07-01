d22ee6bd06e671902c77186499e9ca6e0bc411c6
Lee-0210 <tjsans9069@gmail.com>
2025-07-01 17:51:24 +0900

    중간 커밋

--- resonos/src/main/java/com/cosmus/resonos/controller/UserController.java
+++ resonos/src/main/java/com/cosmus/resonos/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.cosmus.resonos.controller;
 
 import org.springframework.stereotype.Controller;
-
-import groovy.util.logging.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cosmus.resonos.domain.User;
+
+import groovy.util.logging.Slf4j;
+
 
 
 
@@ -16,7 +20,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
   @PostMapping("/login")
-  public String getMethodName(@RequestParam String param) {
+  public String login(@RequestParam String param) {
       return new String();
+  }
+
+  @GetMapping("/mypage")
+  // security 활성화 하면
+  // public String mypage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+  public String mypage(Model model) {
+    User user = User.builder().email("resonos12@gmail.com").username("김조은").build();
+
+    model.addAttribute("user", user);
+    return "user/mypage";
+  }
+
+  @GetMapping("/edit")
+  public String edit() {
+    return "user/edit";
   }
 }
