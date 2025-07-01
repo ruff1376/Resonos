baa70f1a3d6509c7420d1d0d37bf71f8fff1b44a
Lee-0210 <tjsans9069@gmail.com>
2025-07-01 11:23:53 +0900

    스포티파이 추가

--- resonos/SQL/spotify/artist_insert.sql
+++ resonos/SQL/spotify/artist_insert.sql
@@ -10,3 +10,56 @@ INSERT INTO artist (id, name, profile_image, genres) VALUES ('06HL4z0CvFAxyc27GX
 
 -- ==============================================
 
+
+-- 아티스트: Ed Sheeran
+INSERT INTO artist (id, name, profile_image, genres) VALUES ('6eUKZXaKkcviH0Ku9w2n3V', 'Ed Sheeran', 'https://i.scdn.co/image/ab6761610000e5eb399444ed4eace08b549d1161', 'soft pop');
+
+-- ==============================================
+
+
+-- 아티스트: Billie Eilish
+INSERT INTO artist (id, name, profile_image, genres) VALUES ('6qqNVTkY8uBg9cP3Jd7DAH', 'Billie Eilish', 'https://i.scdn.co/image/ab6761610000e5eb4a21b4760d2ecb7b0dcdc8da', '');
+
+-- ==============================================
+
+
+-- 아티스트: Ariana Grande
+INSERT INTO artist (id, name, profile_image, genres) VALUES ('66CXWjxzNUsdJxJ2JdwvnR', 'Ariana Grande', 'https://i.scdn.co/image/ab6761610000e5eb6725802588d7dc1aba076ca5', 'pop');
+
+-- ==============================================
+
+
+-- 아티스트: BTS
+INSERT INTO artist (id, name, profile_image, genres) VALUES ('3Nrfpe0tUJi4K4DXYWgMUX', 'BTS', 'https://i.scdn.co/image/ab6761610000e5ebd642648235ebf3460d2d1f6a', 'k-pop');
+
+-- ==============================================
+
+
+-- 아티스트: BLACKPINK
+INSERT INTO artist (id, name, profile_image, genres) VALUES ('41MozSoPIsD1dJM0CLPjZF', 'BLACKPINK', 'https://i.scdn.co/image/ab6761610000e5eb667b1c37aac06716daa5fe92', 'k-pop');
+
+-- ==============================================
+
+
+-- 아티스트: SEVENTEEN
+INSERT INTO artist (id, name, profile_image, genres) VALUES ('7nqOGRxlXj7N2JYbgNEjYH', 'SEVENTEEN', 'https://i.scdn.co/image/ab6761610000e5eb8da3a229445fd3cd896cdd5c', 'k-pop');
+
+-- ==============================================
+
+
+-- 아티스트: BIGBANG
+INSERT INTO artist (id, name, profile_image, genres) VALUES ('4Kxlr1PRlDKEB0ekOCyHgX', 'BIGBANG', 'https://i.scdn.co/image/ab6761610000e5eb597a4257d0022e2ac837fa7d', 'k-pop');
+
+-- ==============================================
+
+
+-- 아티스트: IVE
+INSERT INTO artist (id, name, profile_image, genres) VALUES ('6RHTUrRF63xao58xh9FXYJ', 'IVE', 'https://i.scdn.co/image/ab6761610000e5eb538dad5b42d963e338726c26', 'k-pop');
+
+-- ==============================================
+
+
+-- 아티스트: Eminem
+INSERT INTO artist (id, name, profile_image, genres) VALUES ('7dGJo4pcD2V6oG8kP0tJRR', 'Eminem', 'https://i.scdn.co/image/ab6761610000e5eba00b11c129b27a88fc72f36b', 'rap, hip hop');
+
+-- ==============================================
