c4a72f69d4819b0e93804a22f9863bb3d04305db
Lee-0210 <tjsans9069@gmail.com>
2025-07-02 15:54:47 +0900

    중간 커밋

--- resonos/src/main/java/com/cosmus/resonos/domain/Playlist.java
+++ resonos/src/main/java/com/cosmus/resonos/domain/Playlist.java
@@ -1,9 +1,16 @@
 package com.cosmus.resonos.domain;
 
 import java.util.Date;
+
+import groovy.transform.builder.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Playlist {
     private Long id;
     private Long userId;
