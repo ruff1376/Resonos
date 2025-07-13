cd4d53e001834c42adc5b8bcc5124e4d932f64c3
GUNW-O-O <rhrjsdn12@naver.com>
2025-07-13 15:47:20 +0900

    아티스트 좋아요기능 해야함

--- resonos/src/main/java/com/cosmus/resonos/controller/ArtistController.java
+++ resonos/src/main/java/com/cosmus/resonos/controller/ArtistController.java
@@ -3,6 +3,7 @@ package com.cosmus.resonos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cosmus.resonos.domain.Album;
 import com.cosmus.resonos.domain.Artist;
+import com.cosmus.resonos.domain.CustomUser;
 import com.cosmus.resonos.domain.Track;
+import com.cosmus.resonos.domain.Users;
 import com.cosmus.resonos.service.AlbumService;
 import com.cosmus.resonos.service.ArtistService;
 import com.cosmus.resonos.service.TrackService;
@@ -25,12 +28,6 @@ import lombok.extern.slf4j.Slf4j;
 public class ArtistController {
 
 
-    // @GetMapping({"/", ""})
-    // public String artist() {
-    //     return "review/artist";
-    // }
-    
-
     @Autowired
     private ArtistService artistService;
 
@@ -40,94 +37,33 @@ public class ArtistController {
     @Autowired
     private TrackService trackService;
 
-    // // 아티스트 목록 화면
-    // @GetMapping
-    // public String list(Model model) throws Exception {
-    //     log.info("[ArtistController] 아티스트 목록 요청");
-    //     List<Artist> artists = artistService.list();
-    //     log.info("[ArtistController] 아티스트 수: {}", artists.size());
-    //     model.addAttribute("artists", artists);
-    //     return "artist/list"; // artist/list.html
-    // }
-
     // 아티스트 화면
     @GetMapping
-    public String artistsInfo(@RequestParam("id") String id, Model model) throws Exception {
-    Artist artist = artistService.selectById(id);
-    List<Album> albums = albumService.findAlbumsByArtistId(id);
-    int albumCount = albumService.countAlbumsByArtist(id);
-    int trackCount = trackService.countTracksByArtist(id);
-    List<Track> top7List = trackService.selectTop7TracksByArtist(id);
-    String mv_url = artistService.selectTopMvUrlByArtist(id);
-    if (artist == null) {
-        return "redirect:/artists?error=notfound";
+    public String artistsInfo(@RequestParam("id") String id, Model model,
+                                @AuthenticationPrincipal CustomUser user) throws Exception {
+        Users loginUser = null;
+        if( user != null ) {
+            loginUser = user.getUser();
+            model.addAttribute("loginUser", loginUser);
+            
+        }
+        Artist artist = artistService.selectById(id);
+        List<Album> albums = albumService.findAlbumsByArtistId(id);
+        int albumCount = albumService.countAlbumsByArtist(id);
+        int trackCount = trackService.countTracksByArtist(id);
+        List<Track> top7List = trackService.selectTop7TracksByArtist(id);
+        String mv_url = artistService.selectTopMvUrlByArtist(id);
+        if (artist == null) {
+            return "redirect:/artists?error=notfound";
+        }
+        model.addAttribute("artist", artist);
+        model.addAttribute("mv", mv_url);
+        model.addAttribute("albums", albums);
+        model.addAttribute("albumCount", albumCount);
+        model.addAttribute("TOP7", top7List);
+        model.addAttribute("trackCount", trackCount);
+        return "review/artist";  // templates/artists/detail.html 뷰 렌더링
     }
-    model.addAttribute("artist", artist);
-    model.addAttribute("mv", mv_url);
-    model.addAttribute("albums", albums);
-    model.addAttribute("albumCount", albumCount);
-    model.addAttribute("TOP7", top7List);
-    model.addAttribute("trackCount", trackCount);
-    return "review/artist";  // templates/artists/detail.html 뷰 렌더링
-}
 
 
-    // // 아티스트 등록 폼
-    // @GetMapping("/new")
-    // public String create(Model model) {
-    //     log.info("[ArtistController] 아티스트 등록 폼 요청");
-    //     model.addAttribute("artist", new Artist());
-    //     return "artist/form"; // artist/form.html
-    // }
-
-    // // 아티스트 등록 처리
-    // @PostMapping
-    // public String createPost(@ModelAttribute Artist artist, Model model) throws Exception {
-    //     log.info("[ArtistController] 아티스트 등록 시도: {}", artist);
-    //     boolean success = artistService.insert(artist);
-    //     if (success) {
-    //         log.info("[ArtistController] 아티스트 등록 성공: {}", artist);
-    //         return "redirect:/artists";
-    //     }
-    //     log.warn("[ArtistController] 아티스트 등록 실패: {}", artist);
-    //     model.addAttribute("error", "등록 실패");
-    //     return "artist/form";
-    // }
-
-    // // 아티스트 수정 폼
-    // @GetMapping("/{id}/edit")
-    // public String update(@PathVariable String id, Model model) throws Exception {
-    //     log.info("[ArtistController] 아티스트 수정 폼 요청 - id: {}", id);
-    //     Artist artist = artistService.select(Integer.valueOf(id));
-    //     if (artist == null) {
-    //         log.warn("[ArtistController] 수정할 아티스트 없음 - id: {}", id);
-    //         return "redirect:/artists?error=notfound";
-    //     }
-    //     model.addAttribute("artist", artist);
-    //     return "artist/form";
-    // }
-
-    // // 아티스트 수정 처리
-    // @PostMapping("/{id}/edit")
-    // public String updatePost(@PathVariable String id, @ModelAttribute Artist artist, Model model) throws Exception {
-    //     log.info("[ArtistController] 아티스트 수정 시도 - id: {}, artist: {}", id, artist);
-    //     artist.setId(id);
-    //     boolean success = artistService.update(artist);
-    //     if (success) {
-    //         log.info("[ArtistController] 아티스트 수정 성공 - id: {}", id);
-    //         return "redirect:/artists/" + id;
-    //     }
-    //     log.warn("[ArtistController] 아티스트 수정 실패 - id: {}", id);
-    //     model.addAttribute("error", "수정 실패");
-    //     return "artist/form";
-    // }
-
-    // // 아티스트 삭제 처리
-    // @PostMapping("/{id}/delete")
-    // public String delete(@PathVariable String id) throws Exception {
-    //     log.info("[ArtistController] 아티스트 삭제 시도 - id: {}", id);
-    //     artistService.delete(id);
-    //     log.info("[ArtistController] 아티스트 삭제 완료 - id: {}", id);
-    //     return "redirect:/artists";
-    // }
 }
