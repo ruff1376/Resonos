11708331b00f592ca8a5c161fd8405acc73303b4
GUNW-O-O <rhrjsdn12@naver.com>
2025-07-24 16:16:34 +0900

    내 커밋을 받아줘

--- resonos/src/main/java/com/cosmus/resonos/domain/RecentReview.java
+++ resonos/src/main/java/com/cosmus/resonos/domain/RecentReview.java
@@ -9,7 +9,7 @@ public class RecentReview {
     private String id; // 트랙이나 앨범의 아이디
     private String name; // 트랙이나 앨범의 타이틀
     private String content; // 리뷰내용
-    private int likeCount; // 해당 리뷰의 좋아요 수
+    private int rating; // 해당 리뷰의 투표된 점수
     private LocalDateTime createdAt; // 리뷰등록 시기
     private String type;   // "TRACK" or "ALBUM"
 }
