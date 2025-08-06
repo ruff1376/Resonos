28056775f3f85d4e242fe30313d1d50007386b4c
ruff1376 <ruff1376@gmail.com>
2025-08-06 17:16:08 +0900

    웰컴, 헤더, 푸터 진행중

--- resonos-app/src/App.jsx
+++ resonos-app/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AdminIndex from './pages/admin/Index';
 import Tailwindtest from './tailwind/test';
 import './styles/index.css'
+import Home from './pages/Home';
 
 const App = () => {
 
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={ <Home/>} />
         <Route path='/admin' element={ <AdminIndex/>} />
         <Route path='/test' element={ <Tailwindtest/>} />
       </Routes>
